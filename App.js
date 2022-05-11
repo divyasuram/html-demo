542222580e2a63f4e88e88444259e4b449e08bb3
divyasuram <75437351+divyasuram@users.noreply.github.com>
2022-05-11 11:57:26 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import About from './components/About';
+import {BrowserRouter,Routes, Route} from 'react-router-dom';
+function App() {
+  return (
+   <BrowserRouter>
+   <Routes>
+     <Route path="/about" element={<About/>}>
+     </Route>
+   </Routes>
+   </BrowserRouter>
+  );
+}
+
+export default App;
