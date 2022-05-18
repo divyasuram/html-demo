77d2d2a0604a6da800300070d8e47b43362aa5fc
divyasuram <75437351+divyasuram@users.noreply.github.com>
2022-05-18 12:56:43 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,15 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
-import About from './components/About';
-import {BrowserRouter,Routes, Route} from 'react-router-dom';
+import {BrowserRouter, Routes, Route } from 'react-router-dom'
+import login from './components/login';
+import register from '/components/register';
+import myProfile from '/components/myProfile';
+import editProfile from '/components/editProfile';
+import posts from '/components/posts';
+ 
+
+
 function App() {
   return (
-   <BrowserRouter>
-   <Routes>
-     <Route path="/about" element={<About/>}>
-     </Route>
-   </Routes>
-   </BrowserRouter>
+    <BrowserRouter>
+    <Routes>
+     <Route path="/login" element={<login/>}></Route>
+     <Route path="/register" element={<register/>}></Route>
+     <Route path="/editProfile" element={<editProfile/>}></Route>
+     <Route path="/myProfile" element={<myProfile/>}></Route>
+     <Route path="/posts" element={<posts/>}></Route>
+     </Routes>
+     </BrowserRouter>
   );
 }
 
